046678aa5485d32c4b2adf559dae7dc24fb0507d
Stasevichd <dg.stasevich@gmail.com>
2021-07-26 14:52:13 +0300

    Create project

---
+++ src/test/java/StartTest.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class StartTest {
+  ChromeDriver wd;
+
+@Test
+  public void initWD(){
+    System.setProperty("webdriver.chrome.driver", "C:\\Projects\\tools\\chromedriver.exe");
+    wd = new ChromeDriver();
+    wd.manage().window().maximize();
+    wd.get("https://service.2fa-chain.kz/");
+  }
+}
